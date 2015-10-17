682f737e5de64bfd8cb951437e4e50483f3120dd
James Hanson <jameshanson94@hotmail.co.uk>
2015-10-17 13:37:23 +0100

    setup devise

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :find_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @posts = Post.all.order('created_at desc').paginate(page: params[:page], per_page: 2)
@@ -17,7 +18,6 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new post_params
-
     if @post.save
       redirect_to @post, notice: "Post was successfully saved."
     else
